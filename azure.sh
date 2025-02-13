ac878ecce89185c930a793eeb39d1fe7d94b161b
Jeff An <theofficialjeffan@gmail.com>
2025-02-12 17:21:06 -0800

    holy bullshit

--- azure.sh
+++ azure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd yarn-example && CI=true npx --verbose momentic@alpha --log-level debug run random-test.test.yaml
+cd yarn-example && CI=true npx --yes --verbose momentic@alpha --log-level debug run random-test.test.yaml
