f0ab093228b9347eca2c71b8c29fb6b200dc80dd
Jeff An <theofficialjeffan@gmail.com>
2025-02-12 17:46:22 -0800

    ??

--- azure.sh
+++ azure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CI=true
 npm config set-shell $(which bash)
-cd yarn-example && CI=true npx --yes --verbose momentic@alpha --log-level debug run random-test.test.yaml
+cd yarn-example && CI=true npx --yes --verbose momentic@1.0.77-alpha.1 --log-level debug run random-test.test.yaml
